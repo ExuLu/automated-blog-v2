4a0c1cb0937684afe7166381c0362911b6a7b064
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 18:05:27 +0000

    chores: install packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,4 +7,3 @@ export default App;
 // 1) Create two components: blog and post
 // 2) Add styles (in simple style-component way)
 // 3) Add router for pages: feed (app/feed), post ( app/posts/:id)
-// 4) Add react query for retrieve id from url and get data
