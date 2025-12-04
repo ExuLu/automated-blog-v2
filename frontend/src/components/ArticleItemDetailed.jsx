f2a2d3f31025e19c919f2d6e4861515ed13cfdd2
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 19:19:53 +0000

    fix: add condition if there is no post

--- frontend/src/components/ArticleItemDetailed.jsx
+++ frontend/src/components/ArticleItemDetailed.jsx
@@ -5,6 +5,14 @@ const ArticleItemDetailed = () => {
   const { id: postId } = useParams();
   const post = mockPosts.find((post) => post.id === Number(postId));
 
+  if (!post)
+    return (
+      <div>
+        <p>Post not found</p>
+        <Link to='/articles'>Back to Main Page</Link>
+      </div>
+    );
+
   return (
     <div>
       <h1>{post.name}</h1>
