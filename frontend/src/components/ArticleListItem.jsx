b0e8ba6aae80a471b0b8fd4a4403e361917357c1
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 19:41:33 +0000

    feat: add shortened text tot article list item

--- frontend/src/components/ArticleListItem.jsx
+++ frontend/src/components/ArticleListItem.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
 const ArticleListItem = ({ post }) => {
+  const shortText = post.text.substring(0, 99);
+
   return (
     <div>
       <h2>{post.name}</h2>
-      <p>{post.text}</p>
+      <p>{`${shortText}...`}</p>
       <Link to={`/articles/${post.id}`}>Read more</Link>
     </div>
   );
