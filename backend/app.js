87333011880531fe98fdd6ffd9d37408f9764344
Alena Sharai <exultantislupus@gmail.com>
2025-12-05 18:34:01 +0000

    feat: add router to express app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const articleRouter = require('./routes/articleRoute');
 
 const app = express();
+
+app.route('/api/articles', articleRouter);
 
 module.exports = app;
--- backend/routes/articleRoute.js
+++ backend/routes/articleRoute.js
@@ -4,3 +4,5 @@ const router = express.Router();
 
 router.route('/').get();
 router.route('/:id').get();
+
+module.exports = router;
