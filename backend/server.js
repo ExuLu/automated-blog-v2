8d3bd5c5419cc2c2acc304d553ca8fefb69727db
Alena Sharai <exultantislupus@gmail.com>
2025-12-08 13:46:42 +0000

    feat: add global errors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
