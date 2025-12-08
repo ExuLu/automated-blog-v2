bdbc5afd91a9f9a9be53e08e88a6a2e29392df06
Alena Sharai <exultantislupus@gmail.com>
2025-12-08 12:27:37 +0000

    refactor: promisify save articles and delete unnecessary try/catch blocks

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,46 +1,25 @@
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const FILE_PATH = path.join(__dirname, '..', 'data', 'articles.json');
 
 let articles = JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8'));
 
-function saveArticlesToFile(articles, res, newArticle) {
+async function saveArticlesToFile(articles) {
   const json = JSON.stringify(articles, null, 2);
 
-  fs.writeFile(FILE_PATH, json, 'utf-8', (err) => {
-    if (err) {
-      articles.pop();
-
-      res.status(500).json({
-        status: 'error',
-        message: 'There was an error while saving an article. Please try again',
-      });
-    }
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        article: newArticle,
-      },
-    });
-  });
+  return await fsPromises.writeFile(FILE_PATH, json, 'utf-8');
 }
 
 exports.getAllArticles = (req, res) => {
-  try {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        articles,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-    });
-  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      articles,
+    },
+  });
 };
 
 exports.getArticleById = (req, res) => {
@@ -53,41 +32,45 @@ exports.getArticleById = (req, res) => {
     });
   }
 
-  try {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        article,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-    });
-  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      article,
+    },
+  });
 };
 
 exports.createArticle = (req, res) => {
-  try {
-    if (!req.body?.title || !req.body?.content) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'The article should contain title and content',
-      });
-    }
-
-    const newArticle = {
-      ...req.body,
-      id: uuidv4(),
-      createdAt: new Date().toISOString(),
-    };
-
-    articles.push(newArticle);
-
-    saveArticlesToFile(articles, res, newArticle);
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
+  if (!req.body?.title || !req.body?.content) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'The article should contain title and content',
     });
   }
+
+  const newArticle = {
+    ...req.body,
+    id: uuidv4(),
+    createdAt: new Date().toISOString(),
+  };
+
+  articles.push(newArticle);
+
+  saveArticlesToFile(articles)
+    .then(() => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          article: newArticle,
+        },
+      });
+    })
+    .catch((err) => {
+      articles.pop();
+
+      res.status(500).json({
+        status: 'error',
+        message: 'There was an error while saving an article. Please try again',
+      });
+    });
 };
