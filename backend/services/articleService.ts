f22132f2ff325ec9b7bf88d80e9a8d685a1d422f
Alena Sharai <exultantislupus@gmail.com>
2026-01-21 14:03:42 +0000

    refactor: create llm client instead of generate article

--- backend/services/articleService.ts
+++ backend/services/articleService.ts
@@ -1,4 +1,4 @@
-import generateArticle from './articleGenerator.js';
+import LlmClient from './llm/llmClient.js';
 import {
   createArticleRepo,
   saveArticleToFile,
@@ -7,24 +7,38 @@ import {
 import { ArticleInput, ArticleRecord } from '../types/article.js';
 import HttpError from '../errors/HttpError.js';
 import { DEFAULT_TOPIC } from '../constants.js';
+import { isDefinedString } from '../validation/isDefinedString.js';
 
-export default async function createAndGenerate(
-  topic?: string,
-): Promise<ArticleRecord> {
-  const generatedData: ArticleInput = await generateArticle(
-    topic || DEFAULT_TOPIC,
-  );
+// export default async function createAndGenerate(
+//   topic?: string,
+// ): Promise<ArticleRecord> {
+//   const apiKey = process.env.OPENROUTER_API_KEY;
+//   const url = process.env.OPENROUTER_URL;
+//   const llmModel = process.env.LLM_MODEL;
 
-  const generatedArticle: ArticleRecord = createArticleRepo(generatedData);
+//   if (
+//     !isDefinedString(apiKey) ||
+//     !isDefinedString(url) ||
+//     !isDefinedString(llmModel)
+//   )
+//     throw new Error('Please add correct environment variables');
 
-  try {
-    await saveArticleToFile();
-  } catch (err) {
-    removeArticleAfterError();
+//   const llmClient = new LlmClient({ model: llmModel, apiKey, url });
+//   const generatedData: ArticleInput = await llmClient.generateArticle(
+//     topic || DEFAULT_TOPIC,
+//   );
 
-    console.error('Failed to save generated article:', err);
-    throw new HttpError(500, 'Failed to save generated article');
-  }
+//   const generatedArticle: ArticleRecord = createArticleRepo(generatedData);
 
-  return generatedArticle;
-}
+//   try {
+//     await saveArticleToFile();
+//   } catch (err) {
+//     removeArticleAfterError();
+
+//     console.error('Failed to save generated article:', err);
+//     throw new HttpError(500, 'Failed to save generated article');
+//   }
+
+//   return generatedArticle;
+// }
+
