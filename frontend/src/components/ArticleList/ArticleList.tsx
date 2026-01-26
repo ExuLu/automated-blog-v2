fddaf6ef9da140c69f2bdf71188f5c79d364d2f4
Alena Sharai <exultantislupus@gmail.com>
2026-01-26 19:09:41 +0000

    refactor: use tanstack query instead of custm hook

--- frontend/src/components/ArticleList/ArticleList.tsx
+++ frontend/src/components/ArticleList/ArticleList.tsx
@@ -10,16 +10,17 @@ const ArticleList = () => {
   const { articles, error, isLoading, refetch } = useArticles();
 
   if (isLoading) return <Spinner />;
-  if (error) return <Error message={error} isMainPage={true} />;
+  if (error) return <Error message={error.message} isMainPage={true} />;
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Automated Blog</h1>
       <GenerateArticleForm submitAction={refetch} />
       <div className={styles.list}>
-        {articles.map((article) => (
-          <ArticleListItem key={article.id} article={article} />
-        ))}
+        {articles &&
+          articles.map((article) => (
+            <ArticleListItem key={article.id} article={article} />
+          ))}
       </div>
     </div>
   );
--- frontend/src/hooks/useArticles.tsx
+++ frontend/src/hooks/useArticles.tsx
@@ -3,35 +3,46 @@ import { getAllArticles } from '../api/articlesApi';
 
 import type { Article } from '../types/article';
 import type { AppError } from '../types/error';
+import { useQuery } from '@tanstack/react-query';
 
-interface UseArticlesResult {
-  articles: Article[];
-  isLoading: boolean;
-  error: string | null;
-  refetch: () => Promise<void>;
-}
+// interface UseArticlesResult {
+//   articles: Article[];
+//   isLoading: boolean;
+//   error: string | null;
+//   refetch: () => Promise<void>;
+// }
 
-export default function useArticles(): UseArticlesResult {
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<AppError>(null);
+// export default function useArticles(): UseArticlesResult {
+//   const [articles, setArticles] = useState<Article[]>([]);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const [error, setError] = useState<AppError>(null);
 
-  const loadArticles = useCallback<() => Promise<void>>(async () => {
-    try {
-      setIsLoading(true);
-      setError(null);
-      const articlesData = await getAllArticles();
-      setArticles(articlesData);
-    } catch (err) {
-      setError(err instanceof Error ? err.message : 'Failed to load articles');
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+//   const loadArticles = useCallback<() => Promise<void>>(async () => {
+//     try {
+//       setIsLoading(true);
+//       setError(null);
+//       const articlesData = await getAllArticles();
+//       setArticles(articlesData);
+//     } catch (err) {
+//       setError(err instanceof Error ? err.message : 'Failed to load articles');
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   }, []);
 
-  useEffect(() => {
-    loadArticles();
-  }, [loadArticles]);
+//   useEffect(() => {
+//     loadArticles();
+//   }, [loadArticles]);
 
-  return { articles, isLoading, error, refetch: loadArticles };
+//   return { articles, isLoading, error, refetch: loadArticles };
+// }
+
+export default function useArticles() {
+  const {
+    data: articles,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: ['cabins'], queryFn: getAllArticles });
+
+  return { articles, error, isLoading };
 }
