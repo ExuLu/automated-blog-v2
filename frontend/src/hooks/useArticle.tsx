d321be3a68290c1d2dd978712a712ea6ff943b64
Alena Sharai <exultantislupus@gmail.com>
2026-01-27 12:50:47 +0000

    feat: add article placeholder from cache of articles

--- frontend/src/hooks/useArticle.tsx
+++ frontend/src/hooks/useArticle.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
 import { getArticleById } from '../api/articlesApi';
@@ -8,15 +8,26 @@ import type { ApiError } from '../types/error';
 
 export default function useArticle() {
   const { id: articleId } = useParams();
+  const queryClient = useQueryClient();
+
+  if (!articleId) {
+    return { article: undefined, error: undefined, isLoading: false };
+  }
 
   const {
     data: article,
     error,
     isLoading,
   } = useQuery<Article, ApiError>({
-    queryKey: ['articles', articleId],
+    queryKey: ['article', articleId],
     queryFn: () => getArticleById(articleId!),
     enabled: !!articleId,
+    staleTime: 30 * 60 * 1000, 
+    gcTime: 60 * 60 * 1000,
+    placeholderData: () => {
+      const articles = queryClient.getQueryData<Article[]>(['articles']);
+      return articles?.find((a) => a.id === articleId);
+    },
   });
 
   return { article, error, isLoading };
